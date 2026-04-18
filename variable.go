38882eb0cb4aadbd6d628fa276146e92703c3380
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2026-04-18 17:06:39 +0900

    feat: 変数の宣言をまとめて、可読性を向上

--- variable.go
+++ variable.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func variable() {
-	var i int = 1
-	var f64 float64 = 1.2
-	var s string = "test"
-	var t, f bool = true, false
+	// var i int = 1
+	// var f64 float64 = 1.2
+	// var s string = "test"
+	// var t, f bool = true, false
+	var (
+		i   int     = 1
+		f64 float64 = 1.2
+		s   string  = "test"
+		t   bool    = true
+		f   bool    = false
+	)
 	fmt.Println(i, f64, s, t, f)
 }
