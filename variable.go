df8969c57a93df8001ac25f461c617fc434a2f39
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2026-04-18 17:10:57 +0900

    feat: 変数の初期化を整理し、可読性を向上

--- variable.go
+++ variable.go
@@ -2,26 +2,34 @@ package main
 
 import "fmt"
 
+// var i int = 1
+// var f64 float64 = 1.2
+// var s string = "test"
+// var t, f bool = true, false
+
+// var (
+// 	i   int
+// 	f64 float64
+// 	s   string
+// 	t   bool
+// 	f   bool
+// )
+
+var (
+	i   int     = 1
+	f64 float64 = 1.2
+	s   string  = "test"
+	t   bool    = true
+	f   bool    = false
+)
+
 func variable() {
-	// var i int = 1
-	// var f64 float64 = 1.2
-	// var s string = "test"
-	// var t, f bool = true, false
-
-	// var (
-	// 	i   int
-	// 	f64 float64
-	// 	s   string
-	// 	t   bool
-	// 	f   bool
-	// )
-
-	var (
-		i   int     = 1
-		f64 float64 = 1.2
-		s   string  = "test"
-		t   bool    = true
-		f   bool    = false
-	)
 	fmt.Println(i, f64, s, t, f)
+
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+
+	fmt.Println(xi, xf64, xs, xt, xf)
 }
