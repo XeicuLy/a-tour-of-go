62c7d71eeb4ff6dfa8d97c3bb06baedc164b8ed4
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2026-04-18 17:08:04 +0900

    feat: 初期値の宣言方法

--- variable.go
+++ variable.go
@@ -7,6 +7,15 @@ func variable() {
 	// var f64 float64 = 1.2
 	// var s string = "test"
 	// var t, f bool = true, false
+
+	// var (
+	// 	i   int
+	// 	f64 float64
+	// 	s   string
+	// 	t   bool
+	// 	f   bool
+	// )
+
 	var (
 		i   int     = 1
 		f64 float64 = 1.2
