c0f9cae9fad88b07c9ae7cdc6aec6f01d3921e0f
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2026-04-18 18:45:17 +0900

    feat: boolFn関数内の論理演算の出力をコメントアウト

--- bool.go
+++ bool.go
@@ -7,15 +7,15 @@ func boolFn() {
 	fmt.Printf("%T %v %t\n", t, 1, t)
 	fmt.Printf("%T %v %t\n", f, 0, f)
 
-	fmt.Println(true && true)   // true
+	// fmt.Println(true && true)   // true
 	fmt.Println(true && false)  // false
 	fmt.Println(false && true)  // false
-	fmt.Println(false && false) // false
+	// fmt.Println(false && false) // false
 
-	fmt.Println(true || true)   // true
+	// fmt.Println(true || true)   // true
 	fmt.Println(true || false)  // true
 	fmt.Println(false || true)  // true
-	fmt.Println(false || false) // false
+	// fmt.Println(false || false) // false
 
 	fmt.Println(!true)  // false
 	fmt.Println(!false) // true
