878d170c4fcc0a539b80fb494dce530a45c56bf1
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2026-04-18 16:58:56 +0900

    feat: 現在のユーザー情報を取得し、表示する機能を追加

--- lesson.go
+++ lesson.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/user"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!", time.Now())
+	currentUser, err := user.Current()
+
+	if err != nil {
+		fmt.Println("Error fetching user:", err)
+		return
+	}
+
+	fmt.Println("Current user:", currentUser.Username)
 }
